4ac9f309702b37046ca990d42e6c6cc27fcf0cc7
NadezhdaShel <nadia344g@gmail.com>
2023-02-07 19:08:03 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let mapConfig = new Map(bracketsConfig);
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+        let isIdentical = str[i] === mapConfig.get(str[i]);
+        if ((mapConfig.has(str[i]) && !isIdentical) || (isIdentical && result.indexOf(str[i]) === -1)) {
+            result.push(str[i]);
+        } else {
+            if ((mapConfig.get(result[result.length - 1]) === str[i] && !isIdentical) || (isIdentical && result.indexOf(str[i]) > -1)) {
+                result.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return (result.length === 0) ? true : false;
 }
